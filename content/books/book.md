43a81aac622716ae151b580423e3e35434433b4a
Ross Whitehouse <ross@MacBookAir.lan>
2026-02-24 13:54:44 +0000

    Added poems

--- content/books/book.md
+++ content/books/book.md
@@ -1,8 +1,6 @@
 ---
-description: This is my first Astro blog post
 title: Oliver Twist
 pubDate: 2026-02-17T00:00:00.000Z
-imgPath: books/olivertwist.jpg
 img: /books/olivertwist.jpg
 ---
 
--- tina/config.js
+++ tina/config.js
@@ -85,6 +85,32 @@ export default defineConfig({
             required: true,
           }
         ]
+      },
+      {
+        name: 'poems',
+        label: 'Poems',
+        path: 'content/poems',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: "datetime",
+            name: "pubDate",
+            label: "Date",
+            required: true,
+          }
+        ]
       }
     ],
   },
